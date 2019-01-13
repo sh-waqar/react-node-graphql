10d3457efa2b202a4401225d511fbb92751ffebb
Waqar Ali <waqar@adeven.com>
2019-01-13 16:25:28 +0100

    Add navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,25 @@ import LoginPage from "./pages/Login";
 import EventsPage from "./pages/Events";
 import BookingsPage from "./pages/Bookings";
 
+import MainNavigation from "./components/Navigation/MainNavigation";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/login" exact />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/login" exact />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./MainNavigation.css";
+
+const MainNavigation = props => {
+  return (
+    <header className="main-navigation">
+      <div className="main-navigation_logo">
+        <h1>The Navbar</h1>
+      </div>
+      <nav className="main-navigation_items">
+        <ul>
+          <li>
+            <NavLink to="/auth">Authenticate</NavLink>
+          </li>
+          <li>
+            <NavLink to="/bookings">Bookings</NavLink>
+          </li>
+          <li>
+            <NavLink to="/events">Events</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
